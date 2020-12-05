1b54c767ae0ad529b8f762a63be561f6b9828311
ingwinlu <winklerlukas+work@gmail.com>
2020-12-05 08:33:25 +0100

    Use math and remove itertools

--- python/05/task.py
+++ python/05/task.py
@@ -1,34 +1,18 @@
-from itertools import tee
-from typing import List
-
-
-def bsp_naive(myrange: List[int], bools: List[bool]):
-    split = int(len(myrange) / 2)
-    if bools[0]:
-        newrange = myrange[split:]
-    else:
-        newrange = myrange[:split]
-    if len(newrange) == 1:
-        return newrange[0]
-    return bsp_naive(newrange, bools[1:])
-
-
 TRUE_MAP = ['B', 'R']
 ROW_COLUMN_INDEX = 7
 
 
-def bp_to_id(bp):
-    binarymapping = [
-        True if char in TRUE_MAP else False
+def bsp(bp):
+    binarymapping = ''.join([
+        "1" if char in TRUE_MAP else "0"
         for char in list(bp)
-    ]
+    ])
+    return int(binarymapping, 2)
 
-    rows = binarymapping[:ROW_COLUMN_INDEX]
-    columns = binarymapping[ROW_COLUMN_INDEX:]
 
-    row = bsp_naive(list(range(128)), rows)
-    column = bsp_naive(list(range(8)), columns)
-
+def bp_to_id(bp):
+    row = bsp(bp[:ROW_COLUMN_INDEX])
+    column = bsp(bp[ROW_COLUMN_INDEX:])
     return row * 8 + column
 
 
@@ -40,17 +24,12 @@ def task1():
     return max_id
 
 
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-
 def task2():
     boardingpasses_combined = open('input.txt').read().splitlines()
     ids = sorted(list(map(bp_to_id, boardingpasses_combined)))
-    for x, y in pairwise(ids):
+    for i in range(0, len(ids)-1):
+        x = ids[i]
+        y = ids[i+1]
         expected = x + 1
         if expected == y:
             continue
