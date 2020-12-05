33ba875dabae2acd2d6d40781c89de35b3f55ed8
ingwinlu <winklerlukas+work@gmail.com>
2020-12-05 09:34:08 +0100

    Make ugly but fast

--- python/05/task.py
+++ python/05/task.py
@@ -1,27 +1,37 @@
 ROW_COLUMN_INDEX = 7
 
 
-def bsp(bp, one='B', zero='F'):
-    binarymapping = bp.replace(one, "1").replace(zero, "0")
-    return int(binarymapping, 2)
+def read_to_bin():
+    return (
+        open('input.txt').read()
+                         .replace("B", "1")
+                         .replace("F", "0")
+                         .replace("R", "1")
+                         .replace("L", "0")
+                         .splitlines()
+    )
 
 
 def bp_to_id(bp):
-    row = bsp(bp[:ROW_COLUMN_INDEX])
-    column = bsp(bp[ROW_COLUMN_INDEX:], 'R', 'L')
+    row = int(
+        bp[:ROW_COLUMN_INDEX], 2
+    )
+    column = int(
+        bp[ROW_COLUMN_INDEX:], 2
+    )
     return row * 8 + column
 
 
 def task1():
-    boardingpasses_combined = open('input.txt')
-    ids = map(bp_to_id, boardingpasses_combined)
+    bp_bins = read_to_bin()
+    ids = map(bp_to_id, bp_bins)
     max_id = max(ids)
     return max_id
 
 
 def task2():
-    boardingpasses_combined = open('input.txt')
-    ids = sorted(list(map(bp_to_id, boardingpasses_combined)))
+    bp_bins = read_to_bin()
+    ids = sorted(list(map(bp_to_id, bp_bins)))
     for i in range(0, len(ids)-1):
         x = ids[i]
         y = ids[i+1]
