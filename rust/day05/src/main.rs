f29307c9b3753c5825c14c55dfd4b261ecfb3a19
ingwinlu <winklerlukas+work@gmail.com>
2020-12-06 20:41:21 +0100

    Speed up by reducing replace calls

--- rust/day05/src/main.rs
+++ rust/day05/src/main.rs
@@ -1,29 +1,28 @@
-use std::io::prelude::*;
-
-fn line_to_id(line: String) -> usize {
-    let binary = line
-        .replace("F", "0")
+fn line_to_bin(line: String) -> String {
+    line.replace("F", "0")
         .replace("B", "1")
         .replace("L", "0")
-        .replace("R", "1");
+        .replace("R", "1")
+}
+
+
+fn bin_to_id(binary: &str) -> usize {
     let row = usize::from_str_radix(&binary[..7], 2).unwrap();
     let column = usize::from_str_radix(&binary[7..], 2).unwrap();
     row * 8 + column
 }
 
 fn task1() -> usize {
-    let file = std::fs::File::open("./input.txt").expect("file open failed");
-    let lines = std::io::BufReader::new(file).lines();
-
-    let ids = lines.map(|line| line_to_id(line.unwrap()));
+    let st_unconverted = std::fs::read_to_string("./input.txt").unwrap();
+    let st_converted = line_to_bin(st_unconverted);
+    let ids = st_converted.lines().map(|line| bin_to_id(line));
     ids.max().unwrap()
 }
 
 fn task2() -> usize {
-    let file = std::fs::File::open("./input.txt").expect("file open failed");
-    let lines = std::io::BufReader::new(file).lines();
-
-    let mut ids: Vec<usize> = lines.map(|line| line_to_id(line.unwrap())).collect();
+    let st_unconverted = std::fs::read_to_string("./input.txt").unwrap();
+    let st_converted = line_to_bin(st_unconverted);
+    let mut ids: Vec<usize> = st_converted.lines().map(|line| bin_to_id(line)).collect();
     ids.sort();
     for i in 0..ids.len() {
         let this =ids[i];
