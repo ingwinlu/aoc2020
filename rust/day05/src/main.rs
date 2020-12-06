327c397c27e165363b67443c71f89d9132918049
ingwinlu <winklerlukas+work@gmail.com>
2020-12-06 20:30:40 +0100

    Impl day05 in rust

---
+++ rust/day05/src/main.rs
@@ -0,0 +1,62 @@
+use std::io::prelude::*;
+
+fn line_to_id(line: String) -> usize {
+    let binary = line
+        .replace("F", "0")
+        .replace("B", "1")
+        .replace("L", "0")
+        .replace("R", "1");
+    let row = usize::from_str_radix(&binary[..7], 2).unwrap();
+    let column = usize::from_str_radix(&binary[7..], 2).unwrap();
+    row * 8 + column
+}
+
+fn task1() -> usize {
+    let file = std::fs::File::open("./input.txt").expect("file open failed");
+    let lines = std::io::BufReader::new(file).lines();
+
+    let ids = lines.map(|line| line_to_id(line.unwrap()));
+    ids.max().unwrap()
+}
+
+fn task2() -> usize {
+    let file = std::fs::File::open("./input.txt").expect("file open failed");
+    let lines = std::io::BufReader::new(file).lines();
+
+    let mut ids: Vec<usize> = lines.map(|line| line_to_id(line.unwrap())).collect();
+    ids.sort();
+    for i in 0..ids.len() {
+        let this =ids[i];
+        let next = ids[i+1];
+        let next_expected = this + 1;
+        if next_expected == next {
+            continue
+        } else {
+            return next_expected
+        }
+    }
+    panic!("Should exit earlier");
+}
+
+fn main() {
+    println!("{}", task1());
+    println!("{}", task2());
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use easybench::bench;
+
+    #[test]
+    fn test_bench_1() {
+        println!("task1: {}", bench(|| task1()));
+        panic!();
+    }
+    #[test]
+    fn test_bench_2() {
+        println!("task2: {}", bench(|| task2()));
+        panic!();
+    }
+}
